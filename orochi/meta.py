c179476f74f28ce77ddca583700dfa5b2b964150
Danilo Bargen <mail@dbrgn.ch>
2014-09-23 22:30:41 +0200

    Fixed problems in MANIFEST.in

--- orochi/meta.py
+++ orochi/meta.py
@@ -1,6 +1,6 @@
 """This module contains metadata about the project."""
 title = 'orochi'
-version = '0.1.0'
+version = '0.1.1'
 description = 'A command line client for 8tracks.com.'
 author = 'Danilo Bargen'
 author_email = 'gezuru@gmail.com'
