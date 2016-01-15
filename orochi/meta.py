b4e5a3d0bf693985f090ddf521441f3dc9c346de
Danilo Bargen <mail@dbrgn.ch>
2016-01-15 11:09:02 +0100

    Set version to v0.2.1-dev

--- orochi/meta.py
+++ orochi/meta.py
@@ -1,6 +1,6 @@
 """This module contains metadata about the project."""
 title = 'orochi'
-version = '0.2.0'
+version = '0.2.1-dev'
 description = 'A command line client for 8tracks.com.'
 author = 'Danilo Bargen'
 author_email = 'gezuru@gmail.com'
